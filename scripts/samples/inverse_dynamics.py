598b9733a79ab78771777311d46fcf911a45ed9e
kindsenior <kunio.einstein@gmail.com>
2025-09-30 13:54:06 +0900

    minner change of inverse dynamics

--- scripts/samples/inverse_dynamics.py
+++ scripts/samples/inverse_dynamics.py
@@ -28,9 +28,9 @@ data.qacc = np.array([0, 0, 0, np.pi/2, 0, 0, 0]) # set qacc after mj_forward
 # 4) apply external force to end effector
 site_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_SITE, "ee")
 body_id = model.site_bodyid[site_id]
-p_world = data.site_xpos[site_id].copy()     # 作用点（ワールド座標）
-F_world = np.array([0.0, 0.0,  50.0])        # 力[N]
-T_world = np.array([0.0, 0.0,  0.0])         # モーメント[Nm]
+p_world = data.site_xpos[site_id].copy() # application point in world frame
+F_world = np.array([0.0, 0.0,  50.0])    # force [N]
+T_world = np.array([0.0, 0.0,  0.0])     # moment [Nm]
 
 # clear external forces (accumulated)
 data.qfrc_applied[:] = 0.0
@@ -50,12 +50,18 @@ Mqacc = np.zeros(model.nv)
 mujoco.mj_mulM(model, data, Mqacc, data.qacc)
 print("M qacc =", Mqacc)
 print("qfrc_bias   =", data.qfrc_bias.copy())
+
+print("\n")
 print("qfrc_applied=", data.qfrc_applied.copy()) # the sum of J^T f
 # qfrc_inverse and the output torque should be the same values (but somehow applied forces are not included)
 print("qfrc_inverse=\n", data.qfrc_inverse.copy())
 tau_jnt = Mqacc + data.qfrc_bias - data.qfrc_applied - data.qfrc_passive - data.qfrc_constraint
 print("M qacc + bias - applied - passive - constraints=\n", tau_jnt)
 print("M qacc + bias - passive - constraints=\n", (tau_jnt + data.qfrc_applied))
+# difference
+print("difference")
+print("qfrc_inverse - (M qacc + bias - passive - constraints)=\n", data.qfrc_inverse - (tau_jnt+ data.qfrc_applied))
+print("qfrc_inverse - (M qacc + bias - applied - passive - constraints)=\n", data.qfrc_inverse - tau_jnt)
 
 # visualize
 viewer.user_scn.ngeom = 0 # reset the start index of geometries
