a38e2635e236abbc3377755b4c1ad2ecfd276a95
kindsenior <kunio.einstein@gmail.com>
2025-12-07 17:14:51 +0900

    modify mass property of the sample bipedal robot

--- src/mujoco_tools/modeling.py
+++ src/mujoco_tools/modeling.py
@@ -50,7 +50,7 @@ def sample_robot_biped(spec, *, prefix=None, free_joint=True, contype=1, conaffi
     if free_joint:
         free_joint = base.add_freejoint() # add a free joint
         free_joint.name = f"{prefix}root"
-    base.add_geom(type=mujoco.mjtGeom.mjGEOM_BOX, size=[0.05, 0.05, 0.02], rgba=gray, contype=contype, conaffinity=conaffinity)
+    base.add_geom(type=mujoco.mjtGeom.mjGEOM_BOX, mass=3.0, size=[0.05, 0.05, 0.02], rgba=gray, contype=contype, conaffinity=conaffinity)
 
     # rleg_link0
     rleg_link0 = base.add_body(name=f"{prefix}rleg_link0", mass=0.5, inertia=[8e-4, 8e-4, 8e-4], pos=[0, -0.1, 0])
@@ -59,20 +59,20 @@ def sample_robot_biped(spec, *, prefix=None, free_joint=True, contype=1, conaffi
     rleg_link1 = rleg_link0.add_body(name=f"{prefix}rleg_link1", mass=0.3, inertia=[4e-4, 4e-4, 4e-4])
     rleg_link1.add_joint(name=f"{prefix}rleg_joint1", type=mujoco.mjtJoint.mjJNT_HINGE, axis=[1, 0, 0], range=[-90, 90])
     # rleg_link2
-    rleg_link2 = rleg_link1.add_body(name=f"{prefix}rleg_link2", mass=0.2, inertia=[2e-4, 2e-4, 2e-4])
+    rleg_link2 = rleg_link1.add_body(name=f"{prefix}rleg_link2")
     rleg_link2.add_joint(name=f"{prefix}rleg_joint2", type=mujoco.mjtJoint.mjJNT_HINGE, axis=[0, 1, 0], range=[-90, 90])
-    rleg_link2.add_geom(type=mujoco.mjtGeom.mjGEOM_BOX, fromto=[0,0,0, 0,0,-0.20], size=[0.02, 0.0, 0.0], rgba=red, contype=contype, conaffinity=conaffinity)
+    rleg_link2.add_geom(type=mujoco.mjtGeom.mjGEOM_BOX, mass=0.2, fromto=[0,0,0, 0,0,-0.20], size=[0.02, 0.0, 0.0], rgba=red, contype=contype, conaffinity=conaffinity)
     # rleg_link3
-    rleg_link3 = rleg_link2.add_body(name=f"{prefix}rleg_link3", mass=0.2, inertia=[2e-4, 2e-4, 2e-4], pos = [0, 0, -0.25])
+    rleg_link3 = rleg_link2.add_body(name=f"{prefix}rleg_link3", pos = [0, 0, -0.25])
     rleg_link3.add_joint(name=f"{prefix}rleg_joint3", type=mujoco.mjtJoint.mjJNT_HINGE, axis=[0, 1, 0], range=[-90, 90])
-    rleg_link3.add_geom(type=mujoco.mjtGeom.mjGEOM_BOX, fromto=[0,0,0, 0,0,-0.20], size=[0.02, 0.0, 0.0], rgba=green, contype=contype, conaffinity=conaffinity)
+    rleg_link3.add_geom(type=mujoco.mjtGeom.mjGEOM_BOX, mass=0.2, fromto=[0,0,0, 0,0,-0.20], size=[0.02, 0.0, 0.0], rgba=green, contype=contype, conaffinity=conaffinity)
     # rleg_link4
     rleg_link4 = rleg_link3.add_body(name=f"{prefix}rleg_link4", mass=0.1, inertia=[1e-4, 1e-4, 1e-4], pos=[0, 0, -0.25])
     rleg_link4.add_joint(name=f"{prefix}rleg_joint4", type=mujoco.mjtJoint.mjJNT_HINGE, axis=[0, 1, 0], range=[-90, 90])
     # rleg_link5
-    rleg_link5 = rleg_link4.add_body(name=f"{prefix}rleg_link5", mass=0.1, inertia=[1e-4, 1e-4, 1e-4])
+    rleg_link5 = rleg_link4.add_body(name=f"{prefix}rleg_link5")
     rleg_link5.add_joint(name=f"{prefix}rleg_joint5", type=mujoco.mjtJoint.mjJNT_HINGE, axis=[1, 0, 0], range=[-90, 90])
-    rleg_link5.add_geom(type=mujoco.mjtGeom.mjGEOM_BOX, fromto=[-0.05,0,0, 0.1,0,0], size=[0.02, 0.0, 0.0], rgba=blue)
+    rleg_link5.add_geom(type=mujoco.mjtGeom.mjGEOM_BOX, mass=0.1, fromto=[0,0,0, 0.1,0,0], size=[0.02, 0.0, 0.0], rgba=blue)
     # right end effector
     rleg_link5.add_site(name=f"{prefix}right_ee", pos=[0.12, 0, 0], size=[0.01, 0.0, 0.0])
 
@@ -83,19 +83,19 @@ def sample_robot_biped(spec, *, prefix=None, free_joint=True, contype=1, conaffi
     lleg_link1 = lleg_link0.add_body(name=f"{prefix}lleg_link1", mass=0.3, inertia=[4e-4, 4e-4, 4e-4])
     lleg_link1.add_joint(name=f"{prefix}lleg_joint1", type=mujoco.mjtJoint.mjJNT_HINGE, axis=[1, 0, 0], range=[-90, 90])
     # lleg_link2
-    lleg_link2 = lleg_link1.add_body(name=f"{prefix}lleg_link2", mass=0.2, inertia=[2e-4, 2e-4, 2e-4])
+    lleg_link2 = lleg_link1.add_body(name=f"{prefix}lleg_link2")
     lleg_link2.add_joint(name=f"{prefix}lleg_joint2", type=mujoco.mjtJoint.mjJNT_HINGE, axis=[0, 1, 0], range=[-90, 90])
-    lleg_link2.add_geom(type=mujoco.mjtGeom.mjGEOM_BOX, fromto=[0,0,0, 0,0,-0.20], size=[0.02, 0.0, 0.0], rgba=red, contype=contype, conaffinity=conaffinity)
+    lleg_link2.add_geom(type=mujoco.mjtGeom.mjGEOM_BOX, mass=0.2, fromto=[0,0,0, 0,0,-0.20], size=[0.02, 0.0, 0.0], rgba=red, contype=contype, conaffinity=conaffinity)
     # lleg_link3
-    lleg_link3 = lleg_link2.add_body(name=f"{prefix}lleg_link3", mass=0.2, inertia=[2e-4, 2e-4, 2e-4], pos = [0, 0, -0.25])
+    lleg_link3 = lleg_link2.add_body(name=f"{prefix}lleg_link3", pos = [0, 0, -0.25])
     lleg_link3.add_joint(name=f"{prefix}lleg_joint3", type=mujoco.mjtJoint.mjJNT_HINGE, axis=[0, 1, 0], range=[-90, 90])
-    lleg_link3.add_geom(type=mujoco.mjtGeom.mjGEOM_BOX, fromto=[0,0,0, 0,0,-0.20], size=[0.02, 0.0, 0.0], rgba=green, contype=contype, conaffinity=conaffinity)
+    lleg_link3.add_geom(type=mujoco.mjtGeom.mjGEOM_BOX, mass=0.2, fromto=[0,0,0, 0,0,-0.20], size=[0.02, 0.0, 0.0], rgba=green, contype=contype, conaffinity=conaffinity)
     # lleg_link4
     lleg_link4 = lleg_link3.add_body(name=f"{prefix}lleg_link4", mass=0.1, inertia=[1e-4, 1e-4, 1e-4], pos=[0, 0, -0.25])
     lleg_link4.add_joint(name=f"{prefix}lleg_joint4", type=mujoco.mjtJoint.mjJNT_HINGE, axis=[0, 1, 0], range=[-90, 90])
     # lleg_link5
-    lleg_link5 = lleg_link4.add_body(name=f"{prefix}lleg_link5", mass=0.1, inertia=[1e-4, 1e-4, 1e-4])
+    lleg_link5 = lleg_link4.add_body(name=f"{prefix}lleg_link5")
     lleg_link5.add_joint(name=f"{prefix}lleg_joint5", type=mujoco.mjtJoint.mjJNT_HINGE, axis=[1, 0, 0], range=[-90, 90])
-    lleg_link5.add_geom(type=mujoco.mjtGeom.mjGEOM_BOX, fromto=[-0.05,0,0, 0.1,0,0], size=[0.02, 0.0, 0.0], rgba=blue, contype=contype, conaffinity=conaffinity)
+    lleg_link5.add_geom(type=mujoco.mjtGeom.mjGEOM_BOX, mass=0.1, fromto=[0,0,0, 0.1,0,0], size=[0.02, 0.0, 0.0], rgba=blue, contype=contype, conaffinity=conaffinity)
     # left end effector
     lleg_link5.add_site(name=f"{prefix}left_ee", pos=[0.12, 0, 0], size=[0.01, 0.0, 0.0])
